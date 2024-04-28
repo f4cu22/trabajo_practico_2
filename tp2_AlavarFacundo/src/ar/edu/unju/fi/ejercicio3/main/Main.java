abaea84a1e87779a1fcf16681e957f48de3136e5
f4cu22 <fascundo96@gmail.com>
2024-04-28 15:10:10 -0300

    Ejercicio 3 terminado

--- tp2_AlavarFacundo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2_AlavarFacundo/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,9 +1,27 @@
 package ar.edu.unju.fi.ejercicio3.main;
+/**
+*Clase Main que representa la informacion
+*de la provincia
+*
+*@author Alavar, Facundo Nicolás
+*@version 1.0
+*
+*/
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Guardar los valores de enum Provincia en una array
+		Provincia[] provincias = Provincia.values();
+		//mostrar cada enum con su informacion
+		for(Provincia provincia : provincias) {
+			System.out.println("Provincia :"+provincia);
+			System.out.println("Poblacion: "+provincia.getPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			System.out.println("Densidad poblacional: "+provincia.calcularDensidadPoblacional());
+			System.out.println();
+		}
 
 	}
 
