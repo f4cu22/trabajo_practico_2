a871efebb235cec556d1df92a4fedec382e8fa22
f4cu22 <fascundo96@gmail.com>
2024-05-01 20:02:13 -0300

    Se crea ejercicio 6, sus paquetes y las clases felinosalvaje y felinodomestico

---
+++ tp2_AlavarFacundo/src/ar/edu/unju/fi/ejercicio6/model/FelinoSalvaje.java
@@ -0,0 +1,59 @@
+package ar.edu.unju.fi.ejercicio6.model;
+
+public class FelinoSalvaje {
+	private String nombre;
+	private byte edad;
+	private float peso;
+	/**
+	 * @param nombre
+	 * @param edad
+	 * @param peso
+	 */
+	public FelinoSalvaje(String nombre, byte edad, float peso) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+	}
+	@Override
+	public String toString() {
+		return "FelinoSalvaje [nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + "]";
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the edad
+	 */
+	public byte getEdad() {
+		return edad;
+	}
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+	/**
+	 * @return the peso
+	 */
+	public float getPeso() {
+		return peso;
+	}
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+}
