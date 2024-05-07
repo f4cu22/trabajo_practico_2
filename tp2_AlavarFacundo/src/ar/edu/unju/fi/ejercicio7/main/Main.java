dbc27883388bbaa9653056519186a3db46257abd
f4cu22 <fascundo96@gmail.com>
2024-05-06 22:33:21 -0300

    se finaliza ejercicio 7

--- tp2_AlavarFacundo/src/ar/edu/unju/fi/ejercicio7/main/Main.java
+++ tp2_AlavarFacundo/src/ar/edu/unju/fi/ejercicio7/main/Main.java
@@ -1,18 +1,26 @@
 package ar.edu.unju.fi.ejercicio7.main;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import ar.edu.unju.fi.ejercicio1.model.Producto;
+import ar.edu.unju.fi.ejercicio1.model.Producto.Categoria;
+import ar.edu.unju.fi.ejercicio1.model.Producto.OrigenFabricacion;
 
 public class Main {
 	
 	public static void main(String[] args) {
-	ArrayList<Producto> productos = new ArrayList<>();
+		ArrayList<Producto> productos = cargarProductos();
+		Scanner scanner = new Scanner(System.in);
+		int opcion;
 	do {
 		mostrarMenu();
-		int opcion = leerOpcion();
+		opcion = leerOpcion(scanner);
+		scanner.nextLine(); // Consumir el carácter de nueva línea
 	switch (opcion){
 		case 1:
 			mostrarProductosDisponibles(productos);
@@ -36,9 +44,47 @@ public class Main {
 			break;
 		default:
 			System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
-		}while(opcion!=7);
-	}
+		}
+	}while(opcion!=7);
+	scanner.close();
 }
+	
+	private static ArrayList<Producto> cargarProductos() {
+    	ArrayList<Producto> productos = new ArrayList<>();
+    	productos.add(new Producto("COD001","HELADERA PHILCO",199.9,OrigenFabricacion.ARGENTINA,Categoria.ELECTRHOGAR,true));
+		productos.add(new Producto("COD002","MONITOR LG",59.9,OrigenFabricacion.BRASIL,Categoria.INFORMATICA,true));
+		productos.add(new Producto("COD003","DESTORNILLADOR PLANO",14.9,OrigenFabricacion.CHILE,Categoria.HERRAMIENTAS,false));
+		productos.add(new Producto("COD004","MATE ROBADO",30.1,OrigenFabricacion.URUGUAY,Categoria.TELEFONIA,true));
+		productos.add(new Producto("COD005","COCINA PHILCO",199.9,OrigenFabricacion.ARGENTINA,Categoria.ELECTRHOGAR,true));
+		productos.add(new Producto("COD006","TECLADO LG",59.9,OrigenFabricacion.BRASIL,Categoria.INFORMATICA,true));
+		productos.add(new Producto("COD007","MARTILLO",14.9,OrigenFabricacion.CHILE,Categoria.HERRAMIENTAS,false));
+		productos.add(new Producto("COD008","AURICULARES",35.1,OrigenFabricacion.URUGUAY,Categoria.TELEFONIA,true));
+		productos.add(new Producto("COD009","MICROONDAS PHILCO",199.9,OrigenFabricacion.ARGENTINA,Categoria.ELECTRHOGAR,true));
+		productos.add(new Producto("COD010","MOUSE LG",59.9,OrigenFabricacion.BRASIL,Categoria.INFORMATICA,true));
+		productos.add(new Producto("COD011","MARTILLO",14.9,OrigenFabricacion.CHILE,Categoria.HERRAMIENTAS,false));
+		productos.add(new Producto("COD012","PROTECTOR DE PANTALLA",30.1,OrigenFabricacion.URUGUAY,Categoria.TELEFONIA,true));
+		productos.add(new Producto("COD013","LAVA BAJILLAS",199.9,OrigenFabricacion.ARGENTINA,Categoria.ELECTRHOGAR,true));
+		productos.add(new Producto("COD014","AURICULARES DEL XOCAS",59.9,OrigenFabricacion.BRASIL,Categoria.INFORMATICA,true));
+		productos.add(new Producto("COD015","DESARMADOR",14.9,OrigenFabricacion.CHILE,Categoria.HERRAMIENTAS,false));
+        return productos;
+    }
+    
+	private static void mostrarMenu() {
+		System.out.println("Menú:");
+		System.out.println("1 – Mostrar productos disponibles");
+		System.out.println("2 – Mostrar productos faltantes");
+		System.out.println("3 – Incrementar precios de los productos en un 20%");
+		System.out.println("4 – Mostrar productos de la categoría Electrohogar disponibles");
+		System.out.println("5 – Ordenar productos por precio de forma descendente");
+		System.out.println("6 - Mostrar nombres de productos en mayúsculas");
+		System.out.println("7 - Salir");
+		System.out.print("Seleccione una opción: ");
+	}
+	
+	private static int leerOpcion(Scanner scanner) {
+		return scanner.nextInt();
+	}
+	
 	private static void mostrarProductos(ArrayList<Producto> productos){
 		//consumer para imprimir cada producto
 		Consumer<Producto> imprimirProducto = p -> System.out.println(p);
@@ -55,38 +101,33 @@ public class Main {
 			}
 		}
 	}
-	
-	private static void mostrarMenu() {
-		System.out.println("");
-	}
-	
-	private static int leerOpcion() {
-		Scanner scanner = new Scanner(System.in);
-		int opcion = scanner.nextInt();
-		return opcion;
-	}
-	
 	private static void mostrarProductosDisponibles(ArrayList<Producto> productos) {
-		
-	}
+		Predicate<Producto> disponibles = Producto::getEstado;
+        mostrarProductos(productos.stream().filter(disponibles).collect(Collectors.toCollection(ArrayList::new)));
+        }
 	
 	private static void mostrarProductosFaltantes(ArrayList<Producto> productos) {
-		
+		Predicate<Producto> faltantes = p -> !p.getEstado();
+		mostrarProductos(productos.stream().filter(faltantes).collect(Collectors.toCollection(ArrayList::new)));
 	}
 	
 	private static ArrayList<Producto> incrementarPrecios(ArrayList<Producto> productos){
-		
-		return null;
+		// Function para incrementar precios
+		productos.forEach(p -> p.setPrecioUnitario(p.getPrecioUnitario() * 1.20));
+		return productos;
 	}
 	private static void mostrarProductosElectrohogarDisponibles(ArrayList<Producto> productos){
-		
+		Predicate<Producto> electrohogarDisponibles = p -> p.getCategoria().equals("Electrohogar") && p.getEstado();
+		mostrarProductos(productos.stream().filter(electrohogarDisponibles).collect(Collectors.toCollection(ArrayList::new)));
 	}
 	
 	private static void ordenarProductosPorPrecioDescendente(ArrayList<Producto> productos){
-		
+		productos.sort(Comparator.comparing(Producto::getPrecioUnitario).reversed());
 	}
 	
 	private static void mostrarNombresEnMayusculas(ArrayList<Producto> productos){
-		
+		// Function para convertir nombres a mayúsculas y mostrarlos
+		Consumer<Producto> imprimirNombreEnMayusculas = p -> System.out.println(p.getDescripcion().toUpperCase());
+	    productos.forEach(imprimirNombreEnMayusculas);
 	}	
 }
